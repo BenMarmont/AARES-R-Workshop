4e06b3dc2fb83c205c456c6313d8eff890135c4e
BenMarmont <bm0782242@gmail.com>
2023-02-01 12:00:54 +1300

    problem solvinng

--- introduction to R for economists.R
+++ introduction to R for economists.R
@@ -1,3 +1,28 @@
+# Troubleshooting with Lee
+# # .libPaths()
+# R_LIBS_USER
+# R_LIBs
+
+# Looks like RTools isnt installed
+# Looks like if admin deletes HTMLtools from programe files I can install fresh
+## As I don't have the ability to write over old program files
+
+assign(".lib.loc", "C:/R_Packages", envir = environment(.libPaths))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # Introduction to R, for Economists
 library(tidyverse)
 ## If there is something you want to do, there's probably a package for it ----
@@ -6,7 +31,8 @@ library(tidyverse)
 
  ## Let this be either the first or the last section 
 
-# Package for downloading XKCD comics 
+# XKCD Data --------------------------------------------------------------------
+# Package for downloading XKCD comics
 #install.packages("XKCDdata")
 library(XKCDdata)
 saved_comic <- get_comic(comic = 2048)
@@ -15,7 +41,7 @@ print_xkcd(comic = 2327)
 
  ##
 
-## Flextable 
+## Flextable--------------------------------------------------------------------
 install.packages("flextable")
 library(flextable)
 mtcars %>% 
@@ -23,9 +49,33 @@ flextable()
 # Flextable isn't installing. Requires new htmltools package. New package won't download for me
 # Will update RStudio and see if I can from there
 
-## gt
+## gt---------------------------------------------------------------------------
 #install.packages("gt")
 library(gt)
 
 mtcars %>%
   gt()
+
+## ts (Time Series)-------------------------------------------------------------
+install.packages("ts")
+library(ts)
+# https://koalatea.io/r-decompose-timeseries/.
+# https://www.datascienceinstitute.net/blog/time-series-decomposition-in-r
+
+# OR use modeltime https://cran.r-project.org/web/packages/modeltime/index.html
+
+install.packages("modeltime")
+
+##
+
+
+## Leaflet----------------------------------------------------------------------
+install.packages("leaflet")
+# https://rstudio.github.io/leaflet/
+# https://cran.r-project.org/web/packages/leaflet.minicharts/vignettes/introduction.html
+
+## 
+
+## sf
+#install.packages("sf")
+library(sf)
